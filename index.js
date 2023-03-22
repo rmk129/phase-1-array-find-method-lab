e00912921ecafb2bb70096115426e849bd9eec58
Romil <romil.m.kothari@gmail.com>
2023-03-22 09:53:13 -0400

    tests completed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+
+function superbowlWin(records) {
+    function isWin(element){
+        if (element.result === "W"){
+            return element;
+        }
+    }
+    let found = records.find(isWin);
+   if (found){
+        return found.year;
+    } 
+    
+}
